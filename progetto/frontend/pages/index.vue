bb7edc2a9f57e2d390b0816aa1a41a7d9aaa5a41
nicodona99 <nicodona99@gmail.com>
2023-05-19 16:44:08 +0200

    prova

--- progetto/frontend/pages/index.vue
+++ progetto/frontend/pages/index.vue
@@ -5,7 +5,7 @@
     <main>
         <img id = "main-img" src = "~/assets/img/home.jpg" />
         <h2>
-            descrizione della VC capital per home page..
+            descrizione della VC capital per home page....
         </h2>
     </main>
 </template>
